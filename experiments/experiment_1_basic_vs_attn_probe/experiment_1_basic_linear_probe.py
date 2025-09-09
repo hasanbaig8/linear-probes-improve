f10e3536811a23af7e69073b1b540e4eb3a6d0e5
Hasan Baig <hasanbaig8@gmail.com>
2025-09-09 11:24:10 +0000

    changing file structure

--- experiments/experiment_1_basic_vs_attn_probe/experiment_1_basic_linear_probe.py
+++ experiments/experiment_1_basic_vs_attn_probe/experiment_1_basic_linear_probe.py
@@ -41,7 +41,9 @@ from torch.nn.functional import one_hot
 
 from dataclasses import dataclass
 
-from typing import List, Tuple
+from typing import List, Tuple, Optional
+
+from enum import Enum
 
 # %% config classes
 @dataclass
@@ -52,29 +54,35 @@ class DataConfig:
     batch_size: int
     llm_name: str
 
+class DataSplit(Enum):
+    TRAIN = "train"
+    VALIDATE = "validate"
+    TEST = "test"
+
 @dataclass
 class ProbeConfig:
     n_epochs: int
     lr: float
-    layer: int #also allow None to train one for each layer
+    layer: int # TODO: also allow None to train one for each layer
     n_classes: int
+    batch_size: int # this is the batch size for logistic probe training and eval
 
 # %% final token activations classes
 
 class PromptDataset(Dataset):
-    def __init__(self, all_train_prompts_df: pl.DataFrame, chosen_path: str, n_samples: int):
-        self.train_prompts_df = all_train_prompts_df.filter(pl.col('path').eq(chosen_path)).head(n_samples)
+    def __init__(self, all_prompts_df: pl.DataFrame, chosen_path: str, n_samples: int):
+        self.prompts_df = all_prompts_df.filter(pl.col('path').eq(chosen_path)).head(n_samples)
     
     def __len__(self):
-        return len(self.train_prompts_df)
+        return len(self.prompts_df)
 
     def __getitem__(self, idx: int):
-        row = self.train_prompts_df[idx]
+        row = self.prompts_df[idx]
         X = row['prompt'].item()
         y = row['shuffled_correct_choice_idx'].item()
         return (X,y)
 
-class FinalTokenActivations(Dataset):
+class FinalTokenActivationsDataset(Dataset):
     def __init__(self, final_token_activations_pairs: List[Tuple[torch.Tensor, torch.Tensor]]):
         '''
         takes in pair of (activations_tensor, correct_idx_tensor)
@@ -97,16 +105,16 @@ class ActivationExtractor:
     def __init__(self, data_config: DataConfig):
         self.data_config = data_config
         # load parquet from disk
-        all_train_prompts_df = pl.read_parquet(self.data_config.train_prompts_file_path)
+        all_prompts_df = pl.read_parquet(self.data_config.train_prompts_file_path)
         
         #filter that down to the chosen path, create the dataset and dataloader
-        self.train_prompt_dataset = PromptDataset(
-            all_train_prompts_df = all_train_prompts_df,
+        self.prompt_dataset = PromptDataset(
+            all_prompts_df = all_prompts_df,
             chosen_path =  self.data_config.chosen_subset_path,
             n_samples = self.data_config.n_samples_train
         )
-        self.train_prompt_loader = DataLoader(
-            dataset = self.train_prompt_dataset,
+        self.prompt_loader = DataLoader(
+            dataset = self.prompt_dataset,
             batch_size=self.data_config.batch_size,
             shuffle=True
         )
@@ -117,7 +125,7 @@ class ActivationExtractor:
     def get_final_token_activations_dataset(self):
         final_token_activations_pairs=[]
         self.llm.eval()
-        for X, y in self.train_prompt_loader:
+        for X, y in self.prompt_loader:
 
             X = [self.llm.tokenizer.apply_chat_template([[
                 {"role": "user", "content": input_str}
@@ -134,7 +142,7 @@ class ActivationExtractor:
 
         #dataset = (torch.concat([x[0] for x in final_token_activations]), torch.concat([x[1] for x in final_token_activations]))
 
-        final_token_activations_dataset = FinalTokenActivations(final_token_activations_pairs)
+        final_token_activations_dataset = FinalTokenActivationsDataset(final_token_activations_pairs)
         return final_token_activations_dataset
 # %% logistic regression classes
 class LogisticClassifier(nn.Module):
@@ -145,20 +153,25 @@ class LogisticClassifier(nn.Module):
     def forward(self,x: torch.Tensor):
         return self.lin1(x)
 
-class ProbeTrainer:
-    def __init__(self, probe_config: ProbeConfig, train_final_tokens_activations_loader: DataLoader):
+class ProbeEvaluator:
+    def __init__(self, probe_config: ProbeConfig, final_tokens_activations_dataset: FinalTokenActivationsDataset, data_split: DataSplit, logistic_classifier: Optional[LogisticClassifier] = None):
         self.probe_config = probe_config
-        self.train_final_tokens_activations_loader = train_final_tokens_activations_loader
-        h = self.train_final_tokens_activations_loader.dataset.activations.shape[-1]
-        self.logistic_classifier = LogisticClassifier(h, probe_config.n_classes).to('cuda')
+        
+        self.final_tokens_activations_loader = DataLoader(dataset=final_tokens_activations_dataset, batch_size = probe_config.batch_size)
+        self.data_split = data_split
+        h = self.final_tokens_activations_loader.dataset.activations.shape[-1]
+        if logistic_classifier is None:
+            self.logistic_classifier = LogisticClassifier(h, probe_config.n_classes).to('cuda')
+        else:
+            self.logistic_classifier = logistic_classifier
         self.optim = Adam(self.logistic_classifier.parameters(),lr=self.probe_config.lr)
         self.loss_fn = CrossEntropyLoss()
 
-    def get_train_accuracy(self):
+    def get_accuracy(self):
         self.logistic_classifier.eval()
         n_correct = 0
         n_total = 0
-        for X, actual in self.train_final_tokens_activations_loader:
+        for X, actual in self.final_tokens_activations_loader:
             preds = self.logistic_classifier(X[:,self.probe_config.layer,:].to('cuda')).argmax(-1)
             n_correct += ((preds == actual.to('cuda')).sum()).cpu().item()
             n_total += len(preds)
@@ -166,16 +179,18 @@ class ProbeTrainer:
         return accuracy
 
     def train_logistic_classifier(self):
+        assert self.data_split == DataSplit.TRAIN
+
         layer = self.probe_config.layer
         for i in range(self.probe_config.n_epochs):
             self.logistic_classifier.train()
             self.logistic_classifier.zero_grad()
-            for X, actual in self.train_final_tokens_activations_loader:
+            for X, actual in self.final_tokens_activations_loader:
                 preds = self.logistic_classifier(X[:,layer,:].to('cuda'))
                 loss = self.loss_fn(preds, actual.to('cuda')).type(torch.float32).to('cuda')
                 loss.backward()
                 self.optim.step()
-            accuracy = self.get_train_accuracy()
+            accuracy = self.get_accuracy()
             print(f'accuracy: {accuracy}')
 
 
@@ -184,26 +199,52 @@ class ProbeTrainer:
 data_config = DataConfig(
     train_prompts_file_path = '/workspace/linear-probes-improve/processed_data/train_prompts.parquet',
     chosen_subset_path = '/workspace/linear-probes-improve/raw_data/105_click_bait',
+    n_samples_train = 1000,
+    batch_size = 8,
+    llm_name = "Qwen/Qwen2.5-1.5B"
+)
+
+
+# %%
+train_activations_extractor = ActivationExtractor(data_config)
+train_final_tokens_activations_dataset = train_activations_extractor.get_final_token_activations_dataset()
+# %%
+train_probe_config = ProbeConfig(
+    n_epochs = 2,
+    lr = 1e-5,
+    layer = -1,
+    n_classes = 2,
+    batch_size = 8,
+)
+train_probe_evaluator = ProbeEvaluator(
+    probe_config = train_probe_config,
+    data_split= DataSplit.TRAIN,
+    final_tokens_activations_dataset=train_final_tokens_activations_dataset
+)
+# %%
+train_probe_evaluator.train_logistic_classifier()
+# %%
+validate_data_config = DataConfig(
+    train_prompts_file_path = '/workspace/linear-probes-improve/processed_data/validate_prompts.parquet',
+    chosen_subset_path = '/workspace/linear-probes-improve/raw_data/105_click_bait',
     n_samples_train = 100,
     batch_size = 8,
     llm_name = "Qwen/Qwen2.5-1.5B"
 )
 
-probe_config = ProbeConfig(
-    n_epochs = 50,
-    lr = 1e-4,
-    layer = -1,
-    n_classes = 2,
-)
+
 # %%
-activations_extractor = ActivationExtractor(data_config)
-train_final_tokens_activations = activations_extractor.get_final_token_activations_dataset()
-train_final_tokens_activations_loader = DataLoader(train_final_tokens_activations, batch_size = 8)
-# %%
-probe_trainer = ProbeTrainer(
-    probe_config = probe_config,
-    train_final_tokens_activations_loader=train_final_tokens_activations_loader
+validate_probe_config = train_probe_config
+validate_activation_extractor = ActivationExtractor(data_config)
+validate_final_tokens_activations_dataset = validate_activation_extractor.get_final_token_activations_dataset()
+validate_probe_evaluator = ProbeEvaluator(
+    probe_config = validate_probe_config,
+    final_tokens_activations_dataset = validate_final_tokens_activations_dataset,
+    data_split = DataSplit.VALIDATE,
+    logistic_classifier = train_probe_evaluator.logistic_classifier
 )
 
-probe_trainer.train_logistic_classifier()
+validate_accuracy = validate_probe_evaluator.get_accuracy()
+
+print(validate_accuracy)
 # %%
